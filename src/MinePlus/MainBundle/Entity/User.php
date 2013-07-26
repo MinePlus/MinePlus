5f31250961100cd2e37ce37df1e62c1d64017133
Malte N <malte.n@live.de>
2013-07-26 13:45:22 +0200

    Create new Use Entity and associated migration.

---
+++ src/MinePlus/MainBundle/Entity/User.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace MinePlus\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * User
+ */
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var boolean
+     */
+    private $admin;
+
+    /**
+     * @var boolean
+     */
+    private $owner;
+
+    /**
+     * @var boolean
+     */
+    private $online;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set admin
+     *
+     * @param boolean $admin
+     * @return User
+     */
+    public function setAdmin($admin)
+    {
+        $this->admin = $admin;
+    
+        return $this;
+    }
+
+    /**
+     * Get admin
+     *
+     * @return boolean 
+     */
+    public function getAdmin()
+    {
+        return $this->admin;
+    }
+
+    /**
+     * Set owner
+     *
+     * @param boolean $owner
+     * @return User
+     */
+    public function setOwner($owner)
+    {
+        $this->owner = $owner;
+    
+        return $this;
+    }
+
+    /**
+     * Get owner
+     *
+     * @return boolean 
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * Set online
+     *
+     * @param boolean $online
+     * @return User
+     */
+    public function setOnline($online)
+    {
+        $this->online = $online;
+    
+        return $this;
+    }
+
+    /**
+     * Get online
+     *
+     * @return boolean 
+     */
+    public function getOnline()
+    {
+        return $this->online;
+    }
+    
+    /*
+     * Get roles
+     * 
+     * TODO: Create a seperate "roles" table, where roles can be dynamically
+     * created and queried.
+     * 
+     * @return array
+     */
+    public function getRoles() 
+    {
+        $roles = array('ROLE_USER');
+        if ($this->getAdmin()) $roles[] = 'ROLE_ADMINISTRATOR';
+        if ($this->getOwner()) $roles[] = 'ROLE_OWNER';
+    }
+    
+    /*
+     * Get salt
+     * 
+     * Since we use sha1 to encrypt the password, we do not need this.
+     * 
+     * @return void
+     */
+    public function getSalt() 
+    {
+        return null;
+    }
+    
+    /*
+     * Erase credentials
+     * 
+     * @return void
+     */
+    public function eraseCredentials()
+    {
+    }
+    
+    /**
+     * @see \Serializable::serialize()
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+        ));
+    }
+
+    /**
+     * @see \Serializable::unserialize()
+     */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+        ) = unserialize($serialized);
+    }
+    
+}
