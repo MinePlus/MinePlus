4c53d8bddbce993d479fe0fd3735d8acf4d9d9a9
Malte N <malte.n@live.de>
2013-07-26 14:09:23 +0200

    Create new SecurityController, associated view, routing and test.

---
+++ src/MinePlus/MainBundle/Controller/SecurityController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MinePlus\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+    }
+
+}
