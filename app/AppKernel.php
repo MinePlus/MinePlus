2968d97c1ac54add7cff5337dafa6f6d1ae31d3c
Malte N <malte.n@live.de>
2013-08-01 17:08:23 +0200

    Create MinePlusWebSocketBundle with Demo Resources
    We should remove this demo resources later.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new MinePlus\MainBundle\MinePlusMainBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new MinePlus\WebSocketBundle\MinePlusWebSocketBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
