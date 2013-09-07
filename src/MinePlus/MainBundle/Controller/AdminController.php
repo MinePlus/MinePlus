384cb6a49332816864a1bdff5a168bf248839179
Malte N <malte.n@live.de>
2013-09-07 14:42:47 +0200

    Create infrastructure for admin settings.
    Note: Also fixed an issue with the user entity.

--- src/MinePlus/MainBundle/Controller/AdminController.php
+++ src/MinePlus/MainBundle/Controller/AdminController.php
@@ -8,6 +8,12 @@ class AdminController extends Controller
 {
     public function indexAction()
     {
+        return $this->render('MinePlusMainBundle:Admin:index.html.twig');
     }
 
+    public function designAction()
+    {
+        return $this->render('MinePlusMainBundle:Admin:design.html.twig');
+    }
+    
 }
