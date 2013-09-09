a9145541221eb5c7c10e598f8555650661e34c11
Malte N <malte.n@live.de>
2013-09-09 19:32:48 +0200

    Fix bug allowing a anonymous user to post on a wall.

--- src/MinePlus/MainBundle/Controller/UserController.php
+++ src/MinePlus/MainBundle/Controller/UserController.php
@@ -3,6 +3,8 @@
 namespace MinePlus\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpFoundation\Request;
 use MinePlus\MainBundle\Entity\Wall;
 use MinePlus\MainBundle\Entity\WallPost;
 use MinePlus\MainBundle\Event\Events;
@@ -34,7 +36,6 @@ class UserController extends Controller
     
     public function showAction($username)
     {
-        $request = $this->getRequest();
         $user = $this->get('fos_user.user_manager')->findUserByUsername($username);
         $manager = $this->getDoctrine()->getManager();
         
@@ -44,29 +45,53 @@ class UserController extends Controller
             $wall->setUser($user);
             
             $manager->persist($wall);
+            $manager->flush();
         } else {
             $wall = $user->getWall();
         }
         
-        if ($request->request->has('submit_wallpost')) { // If post has been submitted
+        if ($this->getRequest()->request->has('submit_wallpost')) {
+            $this->submitWallPost($this->getRequest(), $wall);
+        }
+        
+        return $this->render('MinePlusMainBundle:Player:show.html.twig', array(
+            'user' => $this->get('fos_user.user_manager')->findUserByUsername($username)
+        ));
+    }
+    
+    /*
+     * Creates a wall post from a request.
+     * 
+     * @throws Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException If the user tries to post on a wall but is not logged in.
+     * 
+     * @param Symfony\Component\HttpFoundation\Request $request Request to retrieve wall post data.
+     * @param MinePlus\MainBundle\Entity\Wall $wall The wall to post on.
+     * 
+     * @return boolean True if the post was submitted, false if not.
+     */
+    protected function submitWallPost(Request $request, Wall $wall)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        
+        if ($this->get('security.context')->isGranted('ROLE_USER')) { // If post has been submitted
             $post = new WallPost();
             $post->setWall($wall);
             $post->setUser($this->getUser());
             $post->setMessage($request->request->get('text'));
             $post->setCreated(new \DateTime());
-            
+
             $event = $this->get('event_dispatcher')->dispatch(Events::WALL_POST, new WallPostEvent($post));
-            
-            if (!$event->willBeDeleted()) {
+
+            if (!$event->willBeDeleted()) { // If post may be submitted
                 $manager->persist($event->getPost());
+                $manager->flush();
+                return true;
+            } else {
+                return false;
             }
+        } else {
+            throw new AccessDeniedHttpException();
         }
-        
-        $manager->flush();
-        
-        return $this->render('MinePlusMainBundle:Player:show.html.twig', array(
-            'user' => $this->get('fos_user.user_manager')->findUserByUsername($username)
-        ));
     }
 
 }
