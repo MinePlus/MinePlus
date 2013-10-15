25afc5d04711e3ea569c428e0c0cc4f1b6183d39
Malte N <malte.n@live.de>
2013-10-15 14:27:18 +0200

    Mark the current menu item.

--- src/MinePlus/MainBundle/Menu/BootstrapRenderer.php
+++ src/MinePlus/MainBundle/Menu/BootstrapRenderer.php
@@ -9,15 +9,31 @@ use Knp\Menu\ItemInterface;
 class BootstrapRenderer implements RendererInterface
 {
     
-    protected $engine;
+    protected $engine = null;
+    
+    protected $marker = null;
     
     public function setTemplatingEngine(EngineInterface $engine)
     {
         $this->engine = $engine;
     }
     
+    public function setCurrentMarker(CurrentMarker $marker)
+    {
+        $this->marker = $marker;
+    }
+    
+    /*
+     * @throws \Exception if no templating engine is given.
+     */
     public function render(ItemInterface $item, array $options = array())
     {
+        if (!$this->engine instanceof EngineInterface)
+            throw new \Exception('No valid templating engine is given.');
+            
+        if ($this->marker != null)
+            $item = $this->marker->mark($item);
+        
         $options['menu'] = $item;
         
         return $this->engine->render('MinePlusMainBundle:Menu:bootstrap.html.twig', $options);
---
+++ src/MinePlus/MainBundle/Menu/CurrentMarker.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MinePlus\MainBundle\Menu;
+
+use Knp\Menu\ItemInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class CurrentMarker
+{
+    
+    protected $request;
+    
+    /*
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    
+    /*
+     * Checks whether the url's of the given item and it's childs are the current one.
+     * 
+     * @todo increase depth
+     * 
+     * @param \Knp\Menu\ItemInterface The item to check.
+     * @param bool $markRoot Whether to check the root url, too.
+     * 
+     * @return Knp\Menu\ItemInterface The marked item.
+     */
+    public function mark(ItemInterface $menu, $markRoot = false)
+    {
+        if ($markRoot and $this->isCurrent($menu->getUri()))
+            $menu->setCurrent(true);
+        
+        foreach ($menu as $item) {
+            if ($this->isCurrent($item->getUri()))
+                $item->setCurrent(true);
+        }
+        
+        return $menu;
+    }
+    
+    /*
+     * @param string $url
+     * 
+     * @return bool true if the given url is the current one.
+     */
+    protected function isCurrent($url)
+    {
+        return $url == $this->request->getRequestUri();
+    }
+    
+}
+
+?>
