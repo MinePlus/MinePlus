df519179c8864635ef84e1ffa5769ed3a99cbc56
Malte N <malte.n@live.de>
2013-11-09 20:29:12 +0100

    move common rendering to abstract class Renderer

--- src/MinePlus/MainBundle/Menu/Renderer/MainRenderer.php
+++ src/MinePlus/MainBundle/Menu/Renderer/MainRenderer.php
@@ -2,42 +2,15 @@
 
 namespace MinePlus\MainBundle\Menu\Renderer;
 
-use Symfony\Component\Templating\EngineInterface;
-use Knp\Menu\Renderer\RendererInterface;
-use Knp\Menu\ItemInterface;
-use MinePlus\MainBundle\Menu\CurrentMarker;
-
-class MainRenderer implements RendererInterface
+class MainRenderer extends Renderer
 {
     
-    protected $engine = null;
-    
-    protected $marker = null;
-    
-    public function setTemplatingEngine(EngineInterface $engine)
-    {
-        $this->engine = $engine;
-    }
-    
-    public function setCurrentMarker(CurrentMarker $marker)
-    {
-        $this->marker = $marker;
-    }
-    
     /*
-     * @throws \Exception if no templating engine is given.
+     * {@inheritDoc}
      */
-    public function render(ItemInterface $item, array $options = array())
+    protected function getTemplateName()
     {
-        if (!$this->engine instanceof EngineInterface)
-            throw new \Exception('No valid templating engine is given.');
-            
-        if ($this->marker != null)
-            $item = $this->marker->mark($item);
-        
-        $options['menu'] = $item;
-        
-        return $this->engine->render('MinePlusMainBundle:Menu:main.html.twig', $options);
+        return 'MinePlusMainBundle:Menu:main.html.twig';
     }
     
 }
