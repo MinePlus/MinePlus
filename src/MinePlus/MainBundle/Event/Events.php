3df29cfa70de1b418ecc2c8a6f3df1192e4632a7
Malte N <malte.n@live.de>
2013-11-10 18:03:45 +0100

    differ between user context and menu build events

--- src/MinePlus/MainBundle/Event/Events.php
+++ src/MinePlus/MainBundle/Event/Events.php
@@ -16,6 +16,16 @@ class Events
     const MENU_BUILD = 'menu.build';
     
     /*
+     * Called when the builder renders the user context.
+     * 
+     * The event listener receives an 
+     * MinePlus\MainBundle\Event\Menu\MenuBuildEvent instance.
+     * 
+     * @var string
+     */
+    const USER_CONTEXT_BUILD = 'user_context.build';
+    
+    /*
      * Called BEFORE a post is published to a wall.
      * 
      * The event listener receives an
--- src/MinePlus/MainBundle/Menu/Builder.php
+++ src/MinePlus/MainBundle/Menu/Builder.php
@@ -17,7 +17,7 @@ class Builder extends ContainerAware
         $menu->addChild('dashboard', array('route' => 'dashboard'));
         $menu->addChild('players', array('route' => 'mineplus_main_player_list'));
 
-        return $this->dispatchEvent(new MenuBuildEvent($factory, $menu));
+        return $this->dispatchEvent(new MenuBuildEvent($factory, $menu), Events::MENU_BUILD);
     }
     
     public function userContext(FactoryInterface $factory, array $options)
@@ -26,11 +26,11 @@ class Builder extends ContainerAware
         
         $menu->addChild('logout')->setUri('logout')->setExtra('icon', 'log-out');
 
-        return $this->dispatchEvent(new MenuBuildEvent($factory, $menu));
+        return $this->dispatchEvent(new MenuBuildEvent($factory, $menu), Events::USER_CONTEXT_BUILD);
     }
     
-    protected function dispatchEvent(MenuBuildEvent $event) {
-        $event = $this->container->get('event_dispatcher')->dispatch(Events::MENU_BUILD, $event);
+    protected function dispatchEvent(MenuBuildEvent $event, $name) {
+        $event = $this->container->get('event_dispatcher')->dispatch($name, $event);
         $menu = $event->getMenu();
         
         return $menu;
