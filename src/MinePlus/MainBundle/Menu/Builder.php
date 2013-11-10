db8a2393a2b2bc24ea88878358b523c6d522173e
Malte N <malte.n@live.de>
2013-11-10 17:37:27 +0100

    use KnpMenu for user context

--- src/MinePlus/MainBundle/Menu/Builder.php
+++ src/MinePlus/MainBundle/Menu/Builder.php
@@ -20,6 +20,15 @@ class Builder extends ContainerAware
         return $this->dispatchEvent(new MenuBuildEvent($factory, $menu));
     }
     
+    public function userContext(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('MinePlus');
+        
+        $menu->addChild('logout')->setUri('logout')->setExtra('icon', 'log-out');
+
+        return $this->dispatchEvent(new MenuBuildEvent($factory, $menu));
+    }
+    
     protected function dispatchEvent(MenuBuildEvent $event) {
         $event = $this->container->get('event_dispatcher')->dispatch(Events::MENU_BUILD, $event);
         $menu = $event->getMenu();
--- src/MinePlus/MainBundle/Menu/Renderer/DropdownRenderer.php
+++ src/MinePlus/MainBundle/Menu/Renderer/DropdownRenderer.php
@@ -2,42 +2,12 @@
 
 namespace MinePlus\MainBundle\Menu\Renderer;
 
-use Symfony\Component\Templating\EngineInterface;
-use Knp\Menu\Renderer\RendererInterface;
-use Knp\Menu\ItemInterface;
-use MinePlus\MainBundle\Menu\CurrentMarker;
-
-class MainRenderer implements RendererInterface
+class DropdownRenderer extends Renderer
 {
     
-    protected $engine = null;
-    
-    protected $marker = null;
-    
-    public function setTemplatingEngine(EngineInterface $engine)
+    protected function getTemplateName()
     {
-        $this->engine = $engine;
-    }
-    
-    public function setCurrentMarker(CurrentMarker $marker)
-    {
-        $this->marker = $marker;
-    }
-    
-    /*
-     * @throws \Exception if no templating engine is given.
-     */
-    public function render(ItemInterface $item, array $options = array())
-    {
-        if (!$this->engine instanceof EngineInterface)
-            throw new \Exception('No valid templating engine is given.');
-            
-        if ($this->marker != null)
-            $item = $this->marker->mark($item);
-        
-        $options['menu'] = $item;
-        
-        return $this->engine->render('MinePlusMainBundle:Menu:bootstrap.html.twig', $options);
+        return 'MinePlusMainBundle:Menu:dropdown.html.twig';
     }
     
 }
